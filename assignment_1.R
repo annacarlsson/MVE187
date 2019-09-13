d482b9989344fc873841a4debae89971aa0b4d80
Anna Carlsson <anna.r.carlsson@telia.com>
2019-09-13 08:41:19 +0200

    Create assignment_1.R

---
+++ assignment_1.R
@@ -0,0 +1,12 @@
+#################################################################
+# ASSIGNMENT 1
+# Updated: 2019-09-12
+# Author: Anna Carlsson
+#################################################################
+
+# a) function to plot normal mixture density
+normal_mixture_density <- function(thetas, means, vars, prob_weights){
+  
+  
+  
+}
