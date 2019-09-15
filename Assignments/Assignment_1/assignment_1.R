9cc73ea9a53ab4485c7c22ccacc440bd17cb861a
Anna Carlsson <anna.r.carlsson@telia.com>
2019-09-15 10:52:21 +0200

    Update assignment_1.R

--- Assignments/Assignment_1/assignment_1.R
+++ Assignments/Assignment_1/assignment_1.R
@@ -62,30 +62,30 @@ ggplot() +
 
 #### B) Prior predictive for mixture density ####
 # Parameters
-thetas <- seq(1.51, 1.53, length=1000)
+x <- seq(1.51, 1.53, length=1000)
 weights <- c(0.33, 0.57, 0.10)
 means <- c(1.5163, 1.5197, 1.5203)
 stds <- c(sqrt(0.001^2 + 0.001^2), sqrt(0.001^2 + 0.001^2), sqrt(0.001^2 + 0.005^2))
 
-mixture_prior_density <- normal_mixture_density(thetas, means, stds, weights)
+mixture_prior_density <- normal_mixture_density(x, means, stds, weights)
 
 # Prior predictive densities for each Gaussian
-prior_density_1 <- dnorm(thetas, means[1], stds[1], log = FALSE)
-prior_density_2 <- dnorm(thetas, means[2], stds[2], log = FALSE)
-prior_density_3 <- dnorm(thetas, means[3], stds[3], log = FALSE)
+prior_density_1 <- dnorm(x, means[1], stds[1], log = FALSE)
+prior_density_2 <- dnorm(x, means[2], stds[2], log = FALSE)
+prior_density_3 <- dnorm(x, means[3], stds[3], log = FALSE)
 
 ggplot() +
   geom_line(
-    aes(x = thetas, y = mixture_prior_density), colour = palette[3], size = 1, show.legend = FALSE) + 
+    aes(x = x, y = mixture_prior_density), colour = palette[3], size = 1, show.legend = FALSE) + 
   geom_line(
-    aes(x = thetas, y = prior_density_1), colour = palette[1], size = 1, alpha = 0.25, show.legend = FALSE) + 
+    aes(x = x, y = prior_density_1), colour = palette[1], size = 1, alpha = 0.25, show.legend = FALSE) + 
   geom_line(
-    aes(x = thetas, y = prior_density_2), colour = palette[2], size = 1, alpha = 0.25, show.legend = FALSE) + 
+    aes(x = x, y = prior_density_2), colour = palette[2], size = 1, alpha = 0.25, show.legend = FALSE) + 
   geom_line(
-    aes(x = thetas, y = prior_density_3), colour = palette[4], size = 1, alpha = 0.25, show.legend = FALSE) + 
+    aes(x = x, y = prior_density_3), colour = palette[4], size = 1, alpha = 0.25, show.legend = FALSE) + 
   
-  labs(title = "Prior predictive density for IR (Gaussian mixture)",
-       x = "Theta",
+  labs(title = "Prior predictive density for the measurement x (Gaussian mixture)",
+       x = "x",
        y = "Density") + 
   theme(
     plot.title = element_text(size = 10),
