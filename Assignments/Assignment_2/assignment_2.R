27a45ecb3073fac68b09ba0135c22a23f4ec8e75
Anna Carlsson <anna.r.carlsson@telia.com>
2019-10-03 18:28:15 +0200

    Update assignment_2.R

--- Assignments/Assignment_2/assignment_2.R
+++ Assignments/Assignment_2/assignment_2.R
@@ -155,23 +155,23 @@ estimated_variance <- 1 / N  * sum((P_importance_sample - E_importance_approx)^2
 
 ##### 2A) Maximum likelihood estimate of lambda #####
 Y_i <- c(162, 267, 271, 185, 111, 61, 27, 8, 3, 1)
-i <- seq(from = 0, to = 9, length.out = 10)
+I <- c(0:9)
 
 # MLE of lambda
 n <- sum(Y_i)
-lambda_est <- 1/n * sum(Y_i*i)
+lambda_est <- 1/n * sum(Y_i*I)
 
 # Compute values from distribution
-est_vals <- n * dpois(i, lambda_est, log = FALSE)
+est_vals <- n * dpois(I, lambda_est, log = FALSE)
 
 # Plot distribution with estimated values and actual values
 ggplot() +
   geom_line(
-    aes(x = i, y = est_vals), colour = palette[2], size = 1, show.legend = FALSE) +
+    aes(x = I, y = est_vals), colour = palette[2], size = 1, show.legend = FALSE) +
   geom_point(
-    aes(x = i, y = Y_i), colour = palette[3], size = 1, show.legend = FALSE) +
+    aes(x = I, y = Y_i), colour = palette[3], size = 1, show.legend = FALSE) +
   geom_point(
-    aes(x = i, y = est_vals), colour = palette[2], size = 1, show.legend = FALSE) +
+    aes(x = I, y = est_vals), colour = palette[2], size = 1, show.legend = FALSE) +
   labs(title = "Estimated values and actual values",
        x = "i",
        y = "Density") + 
@@ -185,25 +185,100 @@ ggplot() +
 ##### 2F) Gibb's sampling of posterior of parameters #####
 
 # Initialize matrix
-N <- 1000
-result <- matrix(0, N, 3, byrow=TRUE)
+N <- 20000
+p <- rep(0.5, N)
+lambda_1 <- rep(2, N)
+lambda_2 <- rep(2, N)
 ndata <- length(Y_i)
+
+p_bin <- rep(0, ndata)
 Z_i <- rep(0, ndata)
 
 for (i in 2:N) {
-  #print(i)
-  result[i,1] <- rbeta(1, (1 + ndata * mean(result[i-1, 3])), 1 + 1 + ndata * mean(Y_i - result[i-1, 3]))
-  result[i,2] <- rgamma(1, (1 + sum(seq(0,9) * result[i,1])), (1 + ndata * mean(result[i,1])))
-  result[i,3] <- rgamma(1, (1 + sum(seq(0,9) * (Y_i - result[i,2]))), (1 + ndata * mean(Y_i - result[i,2])))
+  
+  for (j in 1:10){
+    p_bin[j] <- p[i-1] * dpois(j-1, lambda_1[i-1]) / ((p[i-1] * dpois(j-1, lambda_1[i-1]) + (1-p[i-1]) * dpois(j-1, lambda_2[i-1])))
+    Z_i[j] <- rbinom(1, Y_i[j], p_bin[j])
+  }
+
+  p[i] <- rbeta(1, (1 + ndata * mean(Z_i)), 1 + 1 + ndata * mean(Y_i - Z_i))
+  
+  for (j in 1:10){
+    p_bin[j] <- p[i] * dpois(j-1, lambda_1[i-1]) / ((p[i] * dpois(j-1, lambda_1[i-1]) + (1-p[i]) * dpois(j-1, lambda_2[i-1])))
+    Z_i[j] <- rbinom(1, Y_i[j], p_bin[j])
+  }
+  
+  lambda_1[i] <- rgamma(1, (1 + sum(I * Z_i)), (1 + ndata * mean(Z_i)))
+  
+  for (j in 1:10){
+    p_bin[j] <- p[i] * dpois(j-1, lambda_1[i]) / ((p[i] * dpois(j-1, lambda_1[i]) + (1-p[i]) * dpois(j-1, lambda_2[i-1])))
+    Z_i[j] <- rbinom(1, Y_i[j], p_bin[j])
+  }
+  
+  lambda_2[i] <- rgamma(1, (1 + sum(I * (Y_i - Z_i))), (1 + ndata * mean(Y_i - Z_i)))
 }
 
-result <- as.data.frame(result)
-names(result) <- c("p", "lambda_1", "lambda_2")
-plot(seq(1,N), result$p)
-plot(seq(1,N), result$lambda_1)
-plot(seq(1,N), result$lambda_2)
+lambda_1_mean <- mean(lambda_1)
+lambda_2_mean <- mean(lambda_2)
+p_mean <- mean(p)
 
+extended_model <- p_mean * dpois(I, lambda_1_mean) + (1 - p_mean) * dpois(I, lambda_2_mean)
 
+# Plot histograms for each of the parameters
+ggplot() +
+  geom_histogram(
+    aes(x = p), colour = palette[3], fill = palette[3], bins = 50) + 
+  labs(title = "Histogram of p",
+       x = "p",
+       y = "Counts") + 
+  theme(
+    plot.title = element_text(size = 10),
+    axis.title = element_text(size = 10),
+    axis.text = element_text(size = 10),
+    legend.position = c(0.89, 0.85),
+    legend.background = element_rect(fill=alpha('white', 0.8)))
 
+ggplot() +
+  geom_histogram(
+    aes(x = lambda_1), colour = palette[3], fill = palette[3], bins = 50) + 
+  labs(title = "Histogram of lambda_1",
+       x = "lambda_1",
+       y = "Counts") + 
+  theme(
+    plot.title = element_text(size = 10),
+    axis.title = element_text(size = 10),
+    axis.text = element_text(size = 10),
+    legend.position = c(0.89, 0.85),
+    legend.background = element_rect(fill=alpha('white', 0.8)))
 
+ggplot() +
+  geom_histogram(
+    aes(x = lambda_2), colour = palette[3], fill = palette[3], bins = 50) + 
+  labs(title = "Histogram of lambda_2",
+       x = "lambda_2",
+       y = "Counts") + 
+  theme(
+    plot.title = element_text(size = 10),
+    axis.title = element_text(size = 10),
+    axis.text = element_text(size = 10),
+    legend.position = c(0.89, 0.85),
+    legend.background = element_rect(fill=alpha('white', 0.8)))
+
+# Plot the improved density with MLE
+ggplot() +
+  geom_line(
+    aes(x = I, y = est_vals), colour = palette[2], size = 1, show.legend = FALSE) +
+  geom_line(
+    aes(x = I, y = n * extended_model), colour = palette[4], size = 1, show.legend = FALSE) +
+  geom_point(
+    aes(x = I, y = Y_i), colour = palette[3], size = 1, show.legend = FALSE) +
+  labs(title = "MLE density and mixture density",
+       x = "i",
+       y = "Density") + 
+  theme(
+    plot.title = element_text(size = 10),
+    axis.title = element_text(size = 10),
+    axis.text = element_text(size = 10),
+    legend.position = c(0.89, 0.85),
+    legend.background = element_rect(fill=alpha('white', 0.8)))
 
