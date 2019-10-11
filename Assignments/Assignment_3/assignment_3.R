87d1578da50fd35db9bcca70d88a6892d2fb18f8
Anna Carlsson <anna.r.carlsson@telia.com>
2019-10-11 14:32:43 +0200

    Create assignment_3.R

---
+++ Assignments/Assignment_3/assignment_3.R
@@ -0,0 +1,10 @@
+#################################################################
+# ASSIGNMENT 3
+# Updated: 2019-10-11
+# Author: Anna Carlsson
+#################################################################
+
+##### Set working directory and load packages #####
+setwd("/Users/anna/Dokument/GitHub/MVE187/Assignments/Assignment_3")
+library(ggplot2)
+library(gplots)
