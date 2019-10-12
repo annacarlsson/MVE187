3d0b20aad7727fd83bdffac955b4d85c133c78f8
Anna Carlsson <anna.r.carlsson@telia.com>
2019-10-12 12:29:12 +0200

    Update assignment_3.R

--- Assignments/Assignment_3/assignment_3.R
+++ Assignments/Assignment_3/assignment_3.R
@@ -8,3 +8,8 @@
 setwd("/Users/anna/Dokument/GitHub/MVE187/Assignments/Assignment_3")
 library(ggplot2)
 library(gplots)
+
+palette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+set.seed(23563)
+
